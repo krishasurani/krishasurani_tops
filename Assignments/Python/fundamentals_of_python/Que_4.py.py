c5a3523e479588072f047fd0e75ed363d748ebb1
krishasurani <161695548+krishasurani@users.noreply.github.com>
2024-07-17 12:54:32 +0530

    Update Que_4.py.py

--- Assignments/Python/fundamentals_of_python/Que_4.py.py
+++ Assignments/Python/fundamentals_of_python/Que_4.py.py
@@ -1,2 +1,5 @@
 # How memory is managed in Python?
 
+Python uses automatic memory management, which includes a private heap
+containing all Python objects and data structures. 
+Python's memory manager handles allocation and deallocation of memory automatically.
