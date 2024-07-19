8ad6338c24b9887f6048697e2c0c521f912fb0a3
krisha surani <suranikrisha1601@gmail.com>
2024-07-19 22:48:01 +0530

    add files

---
+++ Assignments/Python/fundamentals_of_python/Que_23.py
@@ -0,0 +1,6 @@
+
+# •	Write a Python function to insert a string in the middle of a string.
+
+input_str = input("enter a string: ")
+
+print(input_str.center(50,"-"))
--- Assignments/Python/fundamentals_of_python/Que_4.py.py
+++
@@ -1,2 +0,0 @@
-# How memory is managed in Python?
-
