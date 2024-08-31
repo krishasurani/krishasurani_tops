78f09f787afc6d547899275a88676243854f4647
krishasurani <161695548+krishasurani@users.noreply.github.com>
2024-08-31 20:54:22 +0530

    Delete Assignments/Python/Advance_python_programming directory

--- Assignments/Python/Advance_python_programming/Que_1.py
+++
@@ -1,5 +0,0 @@
-# What is File function in python? What is keywords to create and write file. 
-
-file = open("Que_1.txt",'x')
-file.write("This is question 1")
-file.close()
--- Assignments/Python/Advance_python_programming/Que_7.py
+++
@@ -1,8 +0,0 @@
-# Write a Python program to read a file line by line store it into a variable. 
-
-file = open("7_ques.txt",'r')
-file_list = file.readlines()
-
-for line in file_list:
-    print(line)
-file.close()
